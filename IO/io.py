443e4531ce0cd35ec49bb1b7b7e9423f5e158f1d
nino mal <nino_mrt@hotmail.com>
2024-07-09 17:39:01 -0300

    Update(io): add buton and pop

--- IO/io.py
+++ IO/io.py
@@ -1,30 +1,30 @@
 from tkinter import *
+from tkinter import messagebox
 import random
 
 class IO:
     def __init__(self):
+        self.listData = []
         self.windows = Tk()
         self.windows.title("Diario")
-        self.windows.config(padx= 50, pady= 50)
+        self.windows.config(padx= 20, pady= 10)
         self.canvasImage()
         self.labels()
         self.entrys()
         self.button()
-        
+              
     def canvasImage(self):
         self.canvas = Canvas(height=300, width=600)
-        self.canvas.grid(row= 0, column= 0)
+        self.canvas.grid(row= 0, column= 1)
         self.logo_img = PhotoImage(file= self.randomImagem())
         self.canvas.create_image(100, 100, image = self.logo_img)
         
-    
     def randomImagem(self):
         rng = random.Random()
         randInt = rng.randint(1, 5)
         path = f"IO\image\ess{randInt}.png"
-        print(path)
         return path
-        
+               
     def labels(self):
         self.DIALABEL = Label(text="Dia:")
         self.DIALABEL.grid(row= 1, column= 0)
@@ -41,16 +41,29 @@ class IO:
         self.MES_ENTRY = Entry(width= 20)
         self.MES_ENTRY.grid(row=2, column= 1)
         self.FINURAS_ENTRY = Entry(width= 20)
-        self.FINURAS_ENTRY.grid(row=3, column=1)
+        self.FINURAS_ENTRY.grid(row=3, column= 1)
         self.AGULHAS_ENTRY = Entry(width= 20)
-        self.AGULHAS_ENTRY.grid(row=4, column=1)
+        self.AGULHAS_ENTRY.grid(row=4, column= 1)
 
     def button(self):
-        self.ADDBUTTON = Button(text= "Adicionar", width= 17)
+        self.ADDBUTTON = Button(text= "Adicionar", width= 17, command= self.pop)
         self.ADDBUTTON.grid(row = 5, column= 1)
     
-
-        
+    def pop(self):
+        ask = mensagemBox = messagebox.askyesno("Confirmação", message= "Confirmar os dados")
+        self.askTrue(ask)
+        return ask
+     
+    def askTrue(self, ask):
+        if ask == True:
+            dia = self.DIA_ENTRY.get()   
+            mes = self.MES_ENTRY.get()
+            finura = self.FINURAS_ENTRY.get()
+            agulhas = self.AGULHAS_ENTRY.get()
+            self.addFunc(dia, mes, finura, agulhas)
+                  
+    def addFunc(self, dia, mes, finura , agulha):
+        return self.listData.extend([dia, mes, finura, agulha])
     
     def ioMainLoop(self):
         self.windows.mainloop()
