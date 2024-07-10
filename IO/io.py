cdff32269719b5729ea875b9c936cabfb7dc4a74
nino mal <nino_mrt@hotmail.com>
2024-07-09 21:39:05 -0300

    update(io): add pop finuras

--- IO/io.py
+++ IO/io.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
+from products.Products import Products
 import random
 
 class IO:
     def __init__(self):
+        self.products = Products("01", "07")
         self.listData = []
         self.windows = Tk()
         self.windows.title("Diario")
@@ -11,7 +13,9 @@ class IO:
         self.canvasImage()
         self.labels()
         self.entrys()
-        self.button()
+        self.buttonADD()
+        self.radioButon()
+        self.clearLIstEntrys()
               
     def canvasImage(self):
         self.canvas = Canvas(height=300, width=600)
@@ -45,14 +49,48 @@ class IO:
         self.AGULHAS_ENTRY = Entry(width= 20)
         self.AGULHAS_ENTRY.grid(row=4, column= 1)
 
-    def button(self):
-        self.ADDBUTTON = Button(text= "Adicionar", width= 17, command= self.pop)
+    def buttonADD(self):
+        self.ADDBUTTON = Button(text= "Adicionar", width= 17, command= self.popADD)
         self.ADDBUTTON.grid(row = 5, column= 1)
     
-    def pop(self):
-        ask = mensagemBox = messagebox.askyesno("Confirmação", message= "Confirmar os dados")
-        self.askTrue(ask)
-        return ask
+    def buttonContinue(self):
+        pass #here
+    
+    def buttonPassTurn(self):
+        pass  #here
+        
+    def radioButon(self):
+        turnLabel = Label(text= "TURNO:")
+        turn = StringVar()
+        turn.set("TA")
+        ta = Radiobutton(text="TA",variable= turn, value= "TA")
+        tb = Radiobutton(text="TB",variable= turn, value= "TB")
+        tc = Radiobutton(text="TC",variable= turn, value= "TC")
+
+        turnLabel.grid(row = 1, column=3)
+        ta.grid(row=2, column=3)
+        tb.grid(row=3, column=3)
+        tc.grid(row=4, column=3)  
+        return turn
+        
+    def finurasCheck(self):
+        finura = self.FINURAS_ENTRY.get()
+        asw = self.products.finuraCheck(finura)
+        if not asw:
+            self.popFinuras()
+        else:
+            return True
+    
+    def popFinuras(self):
+        messagebox.showwarning(title="Erro", message= "Finura Errada")
+        self.clearLIstEntrys()
+               
+    def popADD(self):
+        finuras = self.finurasCheck()
+        if finuras :
+            ask = mensagemBox = messagebox.askyesno("Confirmação", message= "Confirmar os dados")
+            self.askTrue(ask)
+            return ask
      
     def askTrue(self, ask):
         if ask == True:
@@ -63,7 +101,14 @@ class IO:
             self.addFunc(dia, mes, finura, agulhas)
                   
     def addFunc(self, dia, mes, finura , agulha):
-        return self.listData.extend([dia, mes, finura, agulha])
+        return self.listData.append([dia, mes, finura, agulha])
+    
+    def clearLIstEntrys(self):
+        self.listData = []
+        self.DIA_ENTRY.delete(0, END)
+        self.MES_ENTRY.delete(0, END)
+        self.FINURAS_ENTRY.delete(0, END)
+        self.AGULHAS_ENTRY.delete(0, END)
     
     def ioMainLoop(self):
         self.windows.mainloop()
